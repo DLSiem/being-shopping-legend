08dcc4ad9e3f9a5b6999d4a6380809717c3c07eb
DLSiem <pdlsiem17@gmail.com>
2024-10-07 19:36:59 +0530

    basic auth api completed

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -46,7 +46,6 @@ export const signInUser = async (req: Request, res: Response) => {
     if (response.rowCount === 0) {
       return res.status(404).json(response);
     }
-
     const comparePassword = await bcrypt.compare(
       password,
       response.data[0].password
@@ -54,24 +53,25 @@ export const signInUser = async (req: Request, res: Response) => {
 
     if (!comparePassword) {
       res.status(401).json({ message: "Invalid credentials" });
+    } else {
+      const token = generateToken(response.data[0].user_id);
+      const user = response.data[0];
+      const filterUser = {
+        user_id: user.user_id,
+        username: user.username,
+        email: user.email,
+        imageUrl: user.image_url,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
+      const filterResponse = {
+        message: "User signed in successfully",
+        data: filterUser,
+        rowCount: response.rowCount,
+      };
+      res.cookie("token", token);
+      return res.status(200).json(filterResponse);
     }
-    const token = generateToken(response.data[0].user_id);
-    const user = response.data[0];
-    const filterUser = {
-      user_id: user.user_id,
-      username: user.username,
-      email: user.email,
-      imageUrl: user.image_url,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
-    const filterResponse = {
-      message: "User signed in successfully",
-      data: filterUser,
-      rowCount: response.rowCount,
-    };
-    res.cookie("token", token);
-    return res.status(200).json(filterResponse);
   } catch (error) {
     console.log(error);
     res.status(500).json({
@@ -82,7 +82,9 @@ export const signInUser = async (req: Request, res: Response) => {
 export const signUpUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).send("Please provide email, username and password");
+    return res
+      .status(400)
+      .json({ message: "Please provide email and password" });
   }
   try {
     const response = await User.getUserByEmail(email);
